9746bade3eaa80279b012174fc2454bd5ee2c9ac
uke <ax.remack@gmail.com>
2021-11-16 18:38:04 +0100

    :sparkles: Using CLI argument to fetch weather

--- src/Main.java
+++ src/Main.java
@@ -7,53 +7,34 @@ import java.util.Date;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String url = "jdbc:sqlite:src/Database/weather.db";
+        CityWeather weather = null;
 
-        DBManager d = new DBManager(url);
+        if (args.length != 1) {
+            System.err.println("Usage : main.java city_name");
+            throw new IllegalArgumentException();
+        }
 
-        d.dropTable();
-        d.createWeatherTable();
+        try {
+            WeatherFetcher weatherFetcher = new WeatherFetcher();
+            weather = weatherFetcher.getWeatherIn(args[0]);
+            System.out.println(weather);
 
-        WeatherFetcher wf1 = new WeatherFetcher();
-        CityWeather weather1 = wf1.getWeatherIn("Clermont");
+            List<Object> list = new ArrayList<>();
+            list.add(weather.getDT());
+            list.add(weather.getCity());
+            list.add(weather.getTemp().getTemp());
+            list.add(weather.getWind().getSpeed());
 
-        List<Object> list1 = new ArrayList<>() {{
-            add(weather1.getDT());
-            add(weather1.getCity());
-            add(weather1.getTemp().getTemp());
-            add(weather1.getWind().getSpeed());
-        }};
-
-        WeatherFetcher wf2 = new WeatherFetcher();
-        CityWeather weather2 = wf2.getWeatherIn("Vichy");
-
-        List<Object> list2 = new ArrayList<>() {{
-            add(weather2.getDT());
-            add(weather2.getCity());
-            add(weather2.getTemp().getTemp());
-            add(weather2.getWind().getSpeed());
-        }};
-
-        WeatherFetcher wf3 = new WeatherFetcher();
-        CityWeather weather3 = wf3.getWeatherIn("Reims");
-
-        List<Object> list3 = new ArrayList<>() {{
-            add(weather3.getDT());
-            add(weather3.getCity());
-            add(weather3.getTemp().getTemp());
-            add(weather3.getWind().getSpeed());
-        }};
-
-        d.insertValues(list1);
-        d.insertValues(list2);
-        d.insertValues(list3);
-
-        System.out.println();
-        d.displayDB();
-
-        d.displayDBOrderedBy("current_temperature");
-
+            DBManager d = new DBManager(url);
+            d.createWeatherTable();
+            d.insertValues(list);
+            d.displayDBOrderedBy("city");
+            d.deleteOldData();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
