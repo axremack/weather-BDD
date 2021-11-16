76188c2dfd8cfab8d27b3f7e7415e0aad68dec55
uke <ax.remack@gmail.com>
2021-11-16 20:06:33 +0100

    :sparkles: Displaying and adding weather fetched in database if it is not already stocked

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import CityWeather.CityWeather;
 import Database.DBManager;
 import WeatherParsing.WeatherFetcher;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -19,30 +18,26 @@ public class Main {
 
         try {
             DBManager d = new DBManager(url);
+            d.deleteOldData(); // Deleting data if too old
+
             boolean found = d.findInDB(args[0]);
 
             if(!found) {
+                WeatherFetcher weatherFetcher = new WeatherFetcher();
+                weather = weatherFetcher.getWeatherIn(args[0]);
+                System.out.println(weather); // Printing out weather fetched
 
+                List<Object> list = new ArrayList<>();
+                list.add(weather.getDT());
+                list.add(weather.getCity());
+                list.add(weather.getTemp().getTemp());
+                list.add(weather.getWind().getSpeed());
+
+                d.createWeatherTable(); // Creating table only if it does not already exist
+                d.insertValues(list);   // Inserting new values in the database
             }
 
-
-            //WeatherFetcher weatherFetcher = new WeatherFetcher();
-            //weather = weatherFetcher.getWeatherIn(args[0]);
-            //System.out.println(weather);
-
-
-            /*List<Object> list = new ArrayList<>();
-            list.add(weather.getDT());
-            list.add(weather.getCity());
-            list.add(weather.getTemp().getTemp());
-            list.add(weather.getWind().getSpeed());*/
-
-
-            /*DBManager d = new DBManager(url);
-            d.createWeatherTable();
-            d.insertValues(list);
             d.displayDBOrderedBy("city");
-            d.deleteOldData();*/
         } catch (Exception e){
             e.printStackTrace();
         }
