4755d5dffd6ffc7246372ad9bc71f7fe0ea4dd26
uke <ax.remack@gmail.com>
2021-11-16 20:58:00 +0100

    :memo: Updating usage print in CLI

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         CityWeather weather = null;
 
         if (args.length != 1) {
-            System.err.println("Usage : main.java city_name");
+            System.err.println("Usage : /usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -classpath /mnt/c/Users/axrem/Documents/ZZ3/Java/TP6/out/production/TP6:/mnt/c/Users/axrem/Downloads/sqlite-jdbc-3.32.3.2.jar:/mnt/c/Users/axrem/Documents/ZZ3/Java/gson-2.8.8.jar Main city_name\n");
             throw new IllegalArgumentException();
         }
 
